cf91dc69f7643b64420569309a354d341a4c4d16
Wenes Gomes Aquino <wenesga@gmail.com>
2023-08-18 11:47:42 -0300

    Atividades de Programação de Computadores UFT - Lincenciatura em Computação - Arraias - TO - 2023 @autor: Wenes Aquino  - 2º Períldo

--- Exemplos/Exemplos Aula07.py
+++ Exemplos/Exemplos Aula07.py
@@ -5,3 +5,23 @@ limpa()#------------------------------------------------------------
     @Autor => Wenes Aquino -> 2º Períldo
 """    #------------------------------------------------------------
 
+# FUNÇÃO 18/08/2023
+
+def sanduiche(nome):
+    print(f'Fazer sanduiche para {nome}')
+    
+def batata(tamanho):
+    print(f'Batata {tamanho}')
+    
+def refrigerante(tipo, tamanho):
+    print(f'{tipo} {tamanho}')
+
+def fazer_combo(nome, tamanhoBat, tipo, tamanhoRefr):
+    sanduiche(nome)
+    batata(tamanhoBat)
+    refrigerante(tipo, tamanhoRefr)
+
+
+fazer_combo('Wenes', 'Grande', 'Coca', 'Pequeno\n')
+fazer_combo('Aquino', 'Medio', 'Peps', 'Grande\n')
+fazer_combo('Gomes', 'Pequeno', 'Fanta', 'Pequeno')
