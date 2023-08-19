f84dcf3be22bb52f78e31a0651998e14d88a0a3f
Wenes Gomes Aquino <wenesga@gmail.com>
2023-08-19 08:05:34 -0300

    Atividades de Programação de Computadores UFT - Lincenciatura em Computação - Arraias - TO - 2023 @autor: Wenes Aquino  - 2º Períldo

--- Exemplos/Exemplos Aula07.py
+++ Exemplos/Exemplos Aula07.py
@@ -7,21 +7,46 @@ limpa()#------------------------------------------------------------
 
 # FUNÇÃO 18/08/2023
 
-def sanduiche(nome):
-    print(f'Fazer sanduiche para {nome}')
+# def sanduiche(nome):
+#     print(f'Fazer sanduiche para {nome}')
     
-def batata(tamanho):
-    print(f'Batata {tamanho}')
+# def batata(tamanho):
+#     print(f'Batata {tamanho}')
     
-def refrigerante(tipo, tamanho):
-    print(f'{tipo} {tamanho}')
+# def refrigerante(tipo, tamanho):
+#     print(f'{tipo} {tamanho}')
 
-def fazer_combo(nome, tamanhoBat, tipo, tamanhoRefr):
-    sanduiche(nome)
-    batata(tamanhoBat)
-    refrigerante(tipo, tamanhoRefr)
+# def fazer_combo(nome, tamanhoBat, tipo, tamanhoRefr):
+#     sanduiche(nome)
+#     batata(tamanhoBat)
+#     refrigerante(tipo, tamanhoRefr)
+
+# fazer_combo('Wenes', 'Grande', 'Coca', 'Pequeno\n')
+# fazer_combo('Aquino', 'Medio', 'Peps', 'Grande\n')
+# fazer_combo('Gomes', 'Pequeno', 'Fanta', 'Pequeno\n')
 
 
-fazer_combo('Wenes', 'Grande', 'Coca', 'Pequeno\n')
-fazer_combo('Aquino', 'Medio', 'Peps', 'Grande\n')
-fazer_combo('Gomes', 'Pequeno', 'Fanta', 'Pequeno')
+# def soma_numero(num1, num2):
+#     return num1 + num2 
+
+# n1 = int(input('Digite1: '))
+# n2 = int(input('Digite2: '))
+
+# print(n1, "+", n2,"=", soma_numero(n1, n2))
+
+
+def mairo_numero(lista_num):
+    lista_num.sort() #ordena numeros
+    lista_num.reverse() #numeros ordem reversa
+    maior_num = lista_num[0] #primeiro numero da lista
+    
+    return maior_num
+
+print(mairo_numero([42,6,812,7,3,1,13,14]))
+
+
+
+
+
+
+
---
+++ Exemplos/Teste simbolo calculo.py
@@ -0,0 +1,17 @@
+limpa = lambda: print("\033[2J\033[;H", end='')
+limpa()
+
+""" #----------------------------------------------------------
+    Arraias - TO - 2023 - UFT - Lincenciatura em Computação
+    Disciplina: Programação de Computadores
+    @Autor: Wenes Aquino => 2º Períldo
+"""#-----------------------------------------------------------
+
+num1 = int(input("Digite1: "))
+num2 = int(input("Digite2: "))
+
+simbolo = str(input("Digite operacao")
+
+soma = num1 + num2
+
+print(soma)
