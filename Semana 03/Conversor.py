a9ec6bc0e2737009a8b7bcbeaa5a714e2b56aec0
Wenes Gomes Aquino <wenesga@gmail.com>
2023-08-25 11:45:59 -0300

    no message

--- Semana 03/Conversor.py
+++ Semana 03/Conversor.py
@@ -13,30 +13,27 @@ moeda_destino = input("Digite a moeda de destino (ex: brl, usd, btc): ")
 
 taxa_dolar = 4.85
 taxa_euro = 5.30
-taxa_bitcoin = 128000
+taxa_bitcoin_brl = 128000
 
 def converte(valor, taxa):
     return valor / taxa
     
-if moeda_origem == 'brl' and moeda_destino == 'usd':
+if moeda_origem == 'brl' and moeda_destino == 'usd': 
     resultado = converte(valor_origem, taxa_dolar)
 
 elif moeda_origem == 'brl' and moeda_destino == 'btc':
-    resultado = converte(valor_origem, taxa_bitcoin)
-
-elif moeda_origem == 'btc' and moeda_destino == 'brl':
-    resultado_btc = converte(valor_origem, taxa_bitcoin)
-    resultado = converte(valor_origem, resultado_btc)
-    print(resultado_btc)
-    print(resultado)
-
-elif moeda_origem == 'btc' and moeda_destino == 'usd':
-    resultado = converte(valor_origem, taxa_dolar)
-
-elif moeda_origem == 'usd' and moeda_destino == 'brl':
-    resultado = converte(valor_origem, valor_origem)
-
-elif moeda_origem == 'usd' and moeda_destino == 'btc':
-    converte(valor_origem, taxa_bitcoin)
+    resultado = converte(valor_origem, taxa_bitcoin_brl)
     
-print(f"Valor convertido = {resultado}")
+elif moeda_origem == 'btc' and moeda_destino == 'brl':
+    resultado = converte(valor_origem * taxa_bitcoin_brl, 1)
+    
+elif moeda_origem == 'btc' and moeda_destino == 'usd':
+    resultado = converte(taxa_bitcoin_brl * valor_origem, taxa_dolar)
+
+elif moeda_origem == 'usd' and moeda_destino == 'brl': 
+    resultado = converte(valor_origem * taxa_dolar, 1)
+
+elif moeda_origem == 'usd' and moeda_destino == 'btc': 
+    resultado = converte(taxa_dolar, taxa_bitcoin_brl) * valor_origem
+    
+print(f"Resultado: {valor_origem} {moeda_origem} = {resultado} {moeda_destino}")
